ed7f2d6814d2d4bbfed30da1b24934580bce9a9a
Observer-L <observer_l@163.com>
2019-03-09 15:59:04 +0800

    mock data

---
+++ src/config.js
@@ -0,0 +1,4 @@
+let DEBUG = true
+const cats = ['Business', 'Academia', 'Government', 'Civic Society']
+const host = 'https://xxx.com'
+export { DEBUG, cats, host }
---
+++ src/utils/http.js
@@ -0,0 +1,32 @@
+import {DEBUG, cats, host} from '../config'
+import Mock from 'mockjs'
+const Random = Mock.Random
+
+export default function http(data = '', cb, method = 'get', header = {}) {
+  if (!DEBUG) {
+    wx.request({
+      url: host + data,
+      method: method ? method : 'get',
+      data: {},
+      header: header ? header : { 'Content-Type': 'application/json' },
+      success(res) {
+        cb(res)
+      }
+    })
+  } else {
+    // 模拟数据
+    const res = Mock.mock({
+      'error_code': '',
+      'error_msg': '',
+      'panels|1-10': [{
+        'id|+1': 1,
+        'img': Random.image(),
+        'cat|1': cats,
+        'title': '@title(3,8)',
+        'place': '@county(true)',
+        'date': '@datetime()'
+      }]
+    })
+    cb(res)
+  }
+}
