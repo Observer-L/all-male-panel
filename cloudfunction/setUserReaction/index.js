59bb1989060d1faf119eb08b61dda722d5fc284e
Observer-L <observer_l@163.com>
2019-03-14 12:25:54 +0800

    fix user reaction logic

--- cloudfunction/setUserReaction/index.js
+++ cloudfunction/setUserReaction/index.js
@@ -8,15 +8,27 @@ const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   const {OPENID, APPID, UNIONID} = cloud.getWXContext()
-  return await
-    db.collection('user_reaction')
-      .where({
-        _openid: _.eq(OPENID),
-        pid: _.eq(event.pid)
-      })
-      .update({
-        data: {
-          reaction: event.reaction
-        }
-      })
+  if (event.init) {
+    return await
+      db.collection('user_reaction')
+        .add({
+          data: {
+            _openid: OPENID,
+            pid: event.pid,
+            reaction: event.reaction
+          }
+        })
+  } else {
+    return await
+      db.collection('user_reaction')
+        .where({
+          _openid: _.eq(OPENID),
+          pid: _.eq(event.pid)
+        })
+        .update({
+          data: {
+            reaction: event.reaction
+          }
+        })
+  }
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 let DEBUG = false
 const cats = ['All', 'Business', 'Academia', 'Government', 'Civic Society']
 const host = 'https://xxx.com'
-export { DEBUG, cats, host }
+const reactions = {
+  angry: 0,
+  disgusting: 0,
+  fine: 0,
+  shock: 0
+}
+export { DEBUG, cats, host, reactions }
